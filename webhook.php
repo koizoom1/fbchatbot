8e8cd4330385102843ee43fc1edc8f13620f7cbc
koizoom1 <jigoku.hen@gmail.com>
2016-06-01 02:39:54 +0900

    validate web hook

--- webhook.php
+++ webhook.php
@@ -1,38 +1,7 @@
 <?php
-$access_token = "EAABZCZBOnpfiMBAGE6NVabf23NP8OVtHVd0ncC4938PSkJf6aWuqW4oZBkxzcndBFCZBXSz27QsvqRAfS8NwLcvZCTHQbgjjEjNAlhum6xF3equpQ6m2y0DuDLPZBnzeggVFqWe2IZA1rzJyMeI21Hg7IQGxf1zqq406HoBEpgmLwZDZD";
-$json_string = file_get_contents('php://input');
-$json_object = json_decode($json_string);
-$messaging = $json_object->entry{0}->messaging{0};
-
-if(isset($messaging->message)) {
-    $id = $messaging->sender->id;
-    $message = 'ねばぎば！';
-    $post = <<< EOM
-    {
-        "recipient":{
-            "id":"{$id}"
-        },
-        "message":{
-            "text":"{$message}"
-        }
-    }
-EOM;
-
-    api_send_request($access_token, $post);
-}
-
-function api_send_request($access_token, $post) {
-    error_log("api_get_message_content_request start");
-    $url = "https://graph.facebook.com/v2.6/me/messages?access_token={$access_token}";
-    $headers = array(
-            "Content-Type: application/json"
-    );
-
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_POST, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    $output = curl_exec($curl);
+$hub_verify_token = "nebagiba"; // 適当なトークンを自分で作成
+if($_GET['hub_verify_token'] == $hub_verify_token) {
+    echo $_GET["hub_challenge"];
+} else {
+    echo 'error';
 }
