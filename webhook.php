2a2fa5feabccfe590e6523a78ba55e262a4e202b
koizoom1 <jigoku.hen@gmail.com>
2016-06-02 04:47:54 +0900

    公式Get Startedを持ってきてテスト

--- webhook.php
+++ webhook.php
@@ -106,6 +106,7 @@ $post = <<< EOM
         "id":"{$from_user_id}"
     },
     "message":{
+	/*
         "attachment":{
             "type":"template",
             "payload":{
@@ -125,6 +126,36 @@ $post = <<< EOM
                 ]
             }
         }
+		*/
+	"attachment": {
+	  "type": "template",
+	  "payload": {
+		"template_type": "generic",
+		"elements": [{
+		  "title": "First card",
+		  "subtitle": "Element #1 of an hscroll",
+		  "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+		  "buttons": [{
+			"type": "web_url",
+			"url": "https://www.messenger.com/",
+			"title": "Web url"
+		  }, {
+			"type": "postback",
+			"title": "Postback",
+			"payload": "Payload for first element in a generic bubble",
+		  }],
+		},{
+		  "title": "Second card",
+		  "subtitle": "Element #2 of an hscroll",
+		  "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+		  "buttons": [{
+			"type": "postback",
+			"title": "Postback",
+			"payload": "Payload for second element in a generic bubble",
+		  }],
+		}]
+	  }
+	}
     }
 }
 EOM;
