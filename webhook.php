207894aa1fa9e4e7c3b6c8e2bafa7c0b5f5def2a
koizoom1 <jigoku.hen@gmail.com>
2016-06-03 12:55:41 +0900

    test for debug

--- webhook.php
+++ webhook.php
@@ -66,7 +66,8 @@ if(isset($messaging->message)) {
     $message =  $messaging->message->text;
 	error_log($message);
 	if( $message == 'じゃんけん' ){
-	api_get_user_profile_request($access_token, $from_user_id);
+	//api_get_user_profile_request($access_token, $from_user_id);
+	api_send_request($access_token, $post,$message);
 	} else {
     $post = <<< EOM
     {
