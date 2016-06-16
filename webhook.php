b1a9f14e8e667996b7b3c112e682ce511453499d
koizoom1 <jigoku.hen@gmail.com>
2016-06-16 20:14:36 +0900

    fix

--- webhook.php
+++ webhook.php
@@ -101,8 +101,8 @@ $post = <<< EOM
               },
               {
                 "type":"postback",
-                "title":"{$csvary[0][8]}",
-                "payload":"{$csvary[0][9]}"
+                "title":"Bookmark Item",
+                "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM100"
               }              
             ]
           }
